5325e3340e184154499998c217e2f43fd2d509f9
Markussim <33492046+Markussim@users.noreply.github.com>
2021-04-27 13:38:11 +0200

    Enabled test

--- code/index.js
+++ code/index.js
@@ -98,4 +98,4 @@ function checkNotAuthenticated(req, res, next) {
   next();
 }
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+if(process.argv[2] != "test") app.listen(port, () => console.log(`Server listening on port ${port}!`));
